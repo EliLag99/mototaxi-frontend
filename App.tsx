94c889b7ce25bbdd8be3b4b182143b72ead7883d
Elias <el2822@ic.ac.uk>
2023-04-26 16:38:47 +0200

    Initial Setup. Mapview + google places API and Routing

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import HomePage from './src/pages/HomePage';
+import ProfilePage from './src/pages/ProfilePage';
+import MorePage from './src/pages/MorePage';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { FontAwesome5 } from '@expo/vector-icons';
+
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Tabs.Navigator
+          initialRouteName="Home"
+          screenOptions={({ route }) => ({
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => {
+              let iconName;
+              if (route.name === 'Home') {
+                iconName = 'home';
+              } else if (route.name === 'Profile') {
+                iconName = "user-circle";
+              } else if (route.name === 'More') {
+                iconName = "plus-square";
+              }
+              return <FontAwesome5 name={iconName} size={size} color={color} />;
+            },
+          })}
+        >
+          <Tabs.Screen name="More" component={MorePage} />
+          <Tabs.Screen name="Home" component={HomePage} />
+          <Tabs.Screen name="Profile" component={ProfilePage} />
+        </Tabs.Navigator>
+      </NavigationContainer >
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
